8867a40d99123acc44b23218d2664ae0f51057b5
Gaurav <gauravt.cdac@gmail.com>
2025-10-31 20:55:31 +0530

    Customer Dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,7 @@ from .validators import allow_only_images_validator
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
@@ -16,27 +17,44 @@ class UserForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
-            raise forms.ValidationError(
-                "Password does not match!"
-            )
+            raise forms.ValidationError("Password does not match!")
 
 
 class UserProfileForm(forms.ModelForm):
-    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
-    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
-    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
-    
-    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     class Meta:
         model = UserProfile
-        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+        fields = [
+            'profile_picture',
+            'cover_photo',
+            'address',
+            'country',
+            'state',
+            'city',
+            'pin_code',
+            'latitude',
+            'longitude'
+        ]
 
     def __init__(self, *args, **kwargs):
         super(UserProfileForm, self).__init__(*args, **kwargs)
-        for field in self.fields:
-            if field == 'latitude' or field == 'longitude':
-                self.fields[field].widget.attrs['readonly'] = 'readonly'
+        self.fields['address'].widget.attrs['placeholder'] = 'Start typing...'
+        self.fields['address'].widget.attrs['required'] = 'required'
+        self.fields['profile_picture'].widget.attrs['class'] = 'btn btn-info'
+        self.fields['cover_photo'].widget.attrs['class'] = 'btn btn-info'
+        for field in ['latitude', 'longitude']:
+            self.fields[field].widget.attrs['readonly'] = 'readonly'
+
+    def clean_profile_picture(self):
+        file = self.cleaned_data.get('profile_picture')
+        if file:
+            allow_only_images_validator(file)
+        return file
+
+    def clean_cover_photo(self):
+        file = self.cleaned_data.get('cover_photo')
+        if file:
+            allow_only_images_validator(file)
+        return file
 
 
 class UserInfoForm(forms.ModelForm):
--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 from accounts.forms import UserInfoForm, UserProfileForm
 from accounts.models import UserProfile
+from orders.models import Order, OrderedFood
+import simplejson as json
+
 
 
 @login_required(login_url='login')
@@ -15,13 +17,14 @@ def cprofile(request):
         user_form = UserInfoForm(request.POST, instance=request.user)
 
         if profile_form.is_valid() and user_form.is_valid():
-            profile_form.save()
             user_form.save()
-            messages.success(request, 'Profile updated successfully.')
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully!')
             return redirect('cprofile')
         else:
             print(profile_form.errors)
             print(user_form.errors)
+            messages.error(request, 'Error updating your profile. Please check the fields.')
     else:
         profile_form = UserProfileForm(instance=profile)
         user_form = UserInfoForm(instance=request.user)
@@ -31,5 +34,34 @@ def cprofile(request):
         'user_form': user_form,
         'profile': profile,
     }
-
     return render(request, 'customers/cprofile.html', context)
+
+
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'customers/my_orders.html', context)
+
+
+def order_detail(request, order_number):
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_food = OrderedFood.objects.filter(order=order)
+        subtotal = 0
+        for item in ordered_food:
+            subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'order': order,
+            'ordered_food': ordered_food,
+            'subtotal': subtotal,
+            'tax_data': tax_data,
+        }
+        return render(request, 'customers/order_detail.html', context)
+    except:
+        return redirect('customer')
+    
